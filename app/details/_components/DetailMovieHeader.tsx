691097c5c6e476fb978ce2b12abe53bd2fc871e3
25LP2243 25LP2243 <25LP2243@G303-CC0130.local>
2025-10-13 12:57:05 +0800

    detail header

--- app/details/_components/DetailMovieHeader.tsx
+++ app/details/_components/DetailMovieHeader.tsx
@@ -1,3 +1,14 @@
-export const DetailMovieHeader = () => {
-  return <div> </div>;
+import { MovieType } from "@/lib/types";
+
+type Props = {
+  movie: MovieType;
+};
+
+export const DetailMovieHeader = ({ movie }: Props) => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">{movie.title}</h1>
+      <p>Rating: {movie.vote_average}</p>
+    </div>
+  );
 };
--- app/details/page.tsx
+++ app/details/page.tsx
@@ -4,11 +4,25 @@ import Image from "next/image";
 import { DetailMovieHeader } from "./_components/DetailMovieHeader";
 import { DetailMovieSection } from "./_components/DetailMovieSection";
 import { DetailMovieInfo } from "./_components/DetailMovieInfo";
+import { MovieType } from "@/lib/types";
+import { axiosInstance } from "@/lib/utils";
+
 export default function Home() {
+  const getMovieDetail = async (movieId: string) => {
+    const response = await axiosInstance(`/movie/${movieId}?language=en-US`);
+    return response.data as MovieType;
+  };
+  const movie = getMovieDetail("1257009");
+
   return (
     <div className="w-100%">
       <Nav />
-      <DetailMovieHeader />
+      <div>
+        <div className="flex w-full flex-wrap gap-6 p-4">
+          <DetailMovieHeader movie={movie} />
+        </div>
+      </div>
+
       <DetailMovieSection />
       <DetailMovieInfo />
       <Footer />
